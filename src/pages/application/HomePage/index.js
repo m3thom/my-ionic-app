4a6e2c5a58247ca576a45c7bad55d8f6b4fea1f6
m3thom <39703347+m3thom@users.noreply.github.com>
2022-01-07 17:27:31 +0700

    feat: Implement basic pushnotification

--- src/pages/application/HomePage/index.js
+++ src/pages/application/HomePage/index.js
@@ -3,13 +3,77 @@ import usersPathHelper from 'helper/pathHelper/users';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleModal } from 'store/slices/application';
 import './Home.scss';
-
+import { PushNotifications } from '@capacitor/push-notifications';
+import { useEffect, useState } from 'react';
+import { Capacitor } from '@capacitor/core';
+     
+const isPushNotificationsAvailable = Capacitor.isPluginAvailable('PushNotifications');
+ 
 const Home = () => {
   const { newUserRegistrationsPath, newUserSessionsPath } = usersPathHelper()
   const applicationState = useSelector(state => state.application)
   const dispatch = useDispatch()
   const handleToggleModal = () => {
     dispatch(toggleModal())
+  }
+  const [notifications, setnotifications] = useState([]);
+
+  useEffect(() => {
+    if (!isPushNotificationsAvailable) return
+
+    PushNotifications.checkPermissions().then((res) => {
+      if (res.receive !== 'granted') {
+        PushNotifications.requestPermissions().then((res) => {
+          if (res.receive === 'denied') {
+            console.log('Push Notification permission denied');
+          }
+          else {
+            console.log('Push Notification permission granted');
+            register();
+          }
+        });
+      }
+      else {
+        register();
+      }
+    });
+  }, [])
+
+  useEffect(() => {
+
+    console.log('notifications', notifications);
+
+  }, [notifications])
+
+  const register = () => {
+    console.log('Initializing HomePage');
+
+    // Register with Apple / Google to receive push via APNS/FCM
+    PushNotifications.register();
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration', (token) => {
+      console.log('Push registration success, token is:', token.value);
+    }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', (error) => {
+      console.log('Error on registration: ' + JSON.stringify(error));
+    }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived', (notification) => {
+      setnotifications(notifications => [...notifications, { id: notification.id, title: notification.title, body: notification.body, type: 'foreground' }])
+    }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification) => {
+      setnotifications(notifications => [...notifications, { id: notification.notification.data.id, title: notification.notification.data.title, body: notification.notification.data.body, type: 'action' }])
+    }
+    );
   }
 
   return (
@@ -37,7 +101,7 @@ const Home = () => {
         </IonList>
         <div>
           <button onClick={handleToggleModal}>Toggle</button>
-          Modal: {applicationState.isModalOpen.toString()}
+          Modal ieie ja: {applicationState.isModalOpen.toString()}
         </div>
       </IonContent>
     </IonPage>
